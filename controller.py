a3e6c6723097b1509cd97ba7bf063e2ce8fa5128
boukaii <guillaume27121993@gmail.com>
2023-06-11 21:33:11 +0200

    p.4

--- controller.py
+++ controller.py
@@ -1,7 +1,6 @@
 from model import Tournament, Match, Round, Player
 from view import ViewMenu, ViewPlayers, ViewTournament, ViewResults, ViewReports
 from utils import load_current_tournament
-from tinydb import TinyDB, Query
 
 
 class TournamentManagement:
@@ -9,19 +8,17 @@ class TournamentManagement:
     def __init__(self):
         self.tournament = None
 
-    test = ViewTournament
-    test1 = ViewPlayers
-    test2 = ViewResults
-    test3 = Tournament
-    test4 = Round
-    test5 = ViewReports
+    view_tournament_global = ViewTournament
+    view_players_global = ViewPlayers
+    view_results_global = ViewResults
+    view_reports_global = ViewReports
 
     def start_tournament(self):
         current_tournament = load_current_tournament()
         if not current_tournament:
-            name, place, date, description, nb_tour, nb_players = self.test.tournament_input()
+            name, place, date, description, nb_tour, nb_players = self.view_tournament_global.tournament_input()
             self.tournament = Tournament(name=name, place=place, date=date, description=description, nb_tour=nb_tour, nb_players=nb_players)
-            self.test.view_tournament_info(self.tournament)
+            self.view_tournament_global.view_tournament_info(self.tournament)
             self.start_type_tournament()
             self.tournament.save()
         else:
@@ -31,15 +28,15 @@ class TournamentManagement:
         self.tournament.save(as_current=False)
 
     def start_type_tournament(self):
-        self.test.type_tournament()
+        self.view_tournament_global.type_tournament()
 
     def create_players(self):
         print("#" * 28 + " " + "Renseignements sur les joueurs " + "#" * 28 + " ")
         for i in range(self.tournament.nb_players - len(self.tournament.players)):
-            first_name, last_name, date_birth, gender = self.test1.player_info()
+            first_name, last_name, date_birth, gender = self.view_players_global.player_info()
             player = Player(first_name=first_name, last_name=last_name, date_birth=date_birth, gender=gender)
             self.tournament.add_player(player)
-            self.test1.view_players_info(player)
+            self.view_players_global.view_players_info(player)
             self.tournament.save()
 
     def create_rounds(self):
@@ -47,7 +44,7 @@ class TournamentManagement:
         for i in range(self.tournament.nb_tour - len(self.tournament.rounds)):
             round_ = Round("round #" + str(i + 1))
             self.tournament.add_round(round_)
-            print(round_.name)
+            # print(round_.name)
         # Divise notre liste de joueur par deux et on trie notre liste de joueur par rang(range définie a 0)
             list_player = sorted(self.tournament.players, key=lambda x: x.score, reverse=True)
             matchs = list()
@@ -56,24 +53,23 @@ class TournamentManagement:
                 player2 = list_player[ia + 1]
                 match = ([player1, 0], [player2, 0])
                 matchs.append(match)
-                print("*" * 30)
-                print(match)
+                # print("*" * 30)
+                # print(match)
                 round_.add_match(Match(player1, player2))
-                print("*" * 100)
-
+                # print("*" * 100)
             for match in round_.matchs:
-                print(match)
-                user_input = self.test5(match)
-                self.save_score(match, user_input)
+                # print(match)
+                user_input = self.result_score(match)
+                self.choice_result(match, user_input)
                 self.tournament.save()
 
-    def test5(self, match):
-        user_input = self.test2.score_match(match)
+    def result_score(self, match):
+        user_input = self.view_results_global.score_match(match)
         while user_input not in ["1", "2", "3"]:
-            user_input = self.test2.score_match(match)
+            user_input = self.view_results_global.score_match(match)
         return user_input
 
-    def save_score(self, match, user_input):
+    def choice_result(self, match, user_input):
         if user_input == "1":
             match.score_player1 = 1
             match.player1.score += 1
@@ -88,33 +84,40 @@ class TournamentManagement:
 
 
 class Report:
-
-    Viewreport = ViewReports
+    view_reports_global = ViewReports
 
     def menu_report(self):
         choice = 1
         print("#" * 28 + " " + "Bienvenue dans le menu des rapports " + " " + "#" * 28)
         while choice != 0:
             choice = int(ViewReports.report_menu())
-            # choix 1 pour afficher les joueurs apar ordre alphabétique
+            # choix 1 pour afficher les joueurs par ordre alphabétique
             if choice == 1:
-                self.get_alphabetic_actors()
+                self.report_player_name()
+            # choix 2 pour afficher les joueurs par classement
             elif choice == 2:
                 pass
+            # choix 2 pour afficher les joueurs par classement
             elif choice == 3:
                 pass
 
-    def get_alphabetic_actors(self, players):
-        players = sorted(players, key=lambda x: x.get('last_name'))
-        self.Viewreport.print_report_players_and_elo(players)
-        print(players)
+    def report_player_name(self):
+        retrieve_tournament = load_current_tournament()
+        print("ooo")
+        retrieve_tournament = sorted(retrieve_tournament["current"]["1"]["players"], key=lambda x: x.get('last_name'))
+        print("aaa")
+        self.view_reports_global.list_players(retrieve_tournament)
+
+    def report_player_score(self):
+        pass
+
+    def report_tournament(self):
+        pass
 
 
 class MenuManagement:
-    tournament1 = TournamentManagement()
-    test = ViewTournament
-    test1 = ViewReports
-    test2 = Report()
+    tournament = TournamentManagement()
+    report_menu = Report()
 
     def main_menu(self):
         choice = 1
@@ -122,11 +125,11 @@ class MenuManagement:
         while choice != 0:
             choice = int(ViewMenu.starting_menu())
             if choice == 1:
-                self.tournament1.start_tournament()
+                self.tournament.start_tournament()
             elif choice == 2:
-                self.tournament1.start_tournament()
+                self.tournament.start_tournament()
             elif choice == 3:
-                self.test2.menu_report()
+                self.report_menu.menu_report()
 
 
 
